aad30c4933d3853cf8d48da8e091bb76cba1e2d1
Rahul Kumar <kumarrahul46700@gmail.com>
2024-08-18 22:48:27 +0530

    minor changes

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -16,19 +16,21 @@ function Modal({ show, child, hide }) {
             id="movable"
             className={`relative w-full md:w-auto md:h-[98%] transition-all delay-75 flex justify-center items-center text-center`}
           >
-            <span>
-              <IoCloseSharp
-                className="text-lighttext absolute top-2 right-[25%] text-sm md:text-3xl cursor-pointer  transition-all "
-                onClick={hide}
-              />
-            </span>
+            
             <span>
               <FaFileDownload className="text-lighttext absolute top-2 left-[25%] text-sm md:text-3xl cursor-pointer  transition-all"></FaFileDownload>
             </span>
             <div
             onClick={() => setZoom(1.2)}
             onDoubleClick={() => setZoom(1)}
-            style={{scale:`${zoom}`}} className="w-full md:w-[40%] transition-all delay-75 ">{child}</div>
+            style={{scale:`${zoom}`}} className="w-[90%] md:w-[40%] transition-all delay-75 ">
+              <span>
+              <IoCloseSharp
+                className="text-lighttext absolute top-2 right-[25%] text-sm md:text-3xl cursor-pointer  transition-all "
+                onClick={hide}
+              />
+            </span>
+              {child}</div>
           </div>
         </div>
       )}
